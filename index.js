c59411ebdc66d8b2112a81fe96c9c4c8635862ac
vudken <vudken@gmail.com>
2025-01-27 17:32:07 +0200

    ..

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const { conf } = require("./src/services/config");
-const { updateData } = require("./src/services/apiService");
-updateData(conf.endpoint.cmd, conf.payload.cmd);
+const { fetchData } = require("./src/services/apiService");
+async function main() {
+    try {
+        const data = await fetchData();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+main();

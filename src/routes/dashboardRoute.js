eb9f8b026d335869966a19fc44deacd7eb5898df
vudken <vudken@gmail.com>
2025-02-04 19:24:08 +0200

    04.02 (fixed dashboard representation of data from API)

--- src/routes/dashboardRoute.js
+++ src/routes/dashboardRoute.js
@@ -3,22 +3,21 @@ const router = express.Router();
 const apiController = require('../controllers/apiController');
 const { isAuthenticated } = require('../middlewares/authMiddleware');
 
-router.get('/home', isAuthenticated, async (req, res) => {
+router.get('/home', async (req, res) => {
     try {
         const data = await apiController.getApiData();
-        // console.log(data);
         // const data = [
         //     { name: 'Tank 1', ph: 6.5, ec: 1.2, description: 'Main tank' },
         //     { name: 'Tank 2', ph: 'invalid', ec: null, description: 'Backup tank' },
         // ];
 
-        // const validatedData = data.map(item => ({
-        //     ...item,
-        //     ph: isNaN(item.ph) ? 0 : parseFloat(item.ph),
-        //     ec: isNaN(item.ec) ? 0 : parseFloat(item.ec),
-        // }));
+        const validatedData = data.map(item => ({
+            ...item,
+            ph: item.ph == "N/A" ? item.ph : parseFloat(item.ph),
+            ec: item.ec == "N/A" ? item.ec : parseFloat(item.ec),
+        }));
 
-        res.render('dashboard', { section: 'home', data });
+        res.render('dashboard', { section: 'home', data: validatedData });
     } catch (error) {
         console.error('Error fetching data:', error.message);
         res.render('dashboard', { section: 'home', data: [] });
